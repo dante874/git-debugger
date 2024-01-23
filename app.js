ef0400b9701e3ad8b8b5e9ac6d879cefa576c68b
dante <dantedros333@gmail.com>
2024-01-23 15:57:59 +0000

    this is debugger ex

---
+++ app.js
@@ -0,0 +1,24 @@
+let x;
+let y;
+
+
+x = Math.min(1);
+x = Math.max(100);
+x = Math.floor(Math.random() * 100);
+
+y = Math.min(1);
+y = Math.max(50);
+y = Math.floor(Math.random() * 50);
+let myVariable = (x, y);
+sumOutput = x + y
+console.log(sumOutput);
+diffOutput = x - y
+let diff = `${x} - ${y} = ${diffOutput}`;
+console.log(diff);
+console.log(diffOutput);
+const myVariableDe = "yada yada yada";
+debugger;
+prodOutput = x * y
+console.log(prodOutput);
+quotientOutput = x / y
+console.log(quotientOutput);
